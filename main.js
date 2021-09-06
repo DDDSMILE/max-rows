29337602867cf5b0ef667687d5c5bb385cd8d5be
DDDSMILE <thanhdat9521@gmail.com>
2021-09-06 18:15:03 +0700

    update

---
+++ main.js
@@ -0,0 +1,25 @@
+var observe;
+
+    observe = function (element, event, handler) {
+        element.addEventListener(event, handler, false);
+    };
+function init () {
+    var text = document.getElementById('text');
+    function resize () {
+        text.style.height = 'auto';
+        text.style.height = text.scrollHeight+'px';
+    }
+    text.style.maxHeight= text.scrollHeight*5+'px';
+
+    /* 0-timeout to get the already changed text */
+    function delayedResize () {
+        window.setTimeout(resize, 0);
+    }
+    observe(text, 'change',  resize);
+    observe(text, 'cut',     delayedResize);
+    observe(text, 'paste',   delayedResize);
+    observe(text, 'drop',    delayedResize);
+    observe(text, 'keydown', delayedResize);
+
+    resize();
+}
